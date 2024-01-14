538749f845737eac18ba7f4878dd686d6abca974
Surojit Paul <surojit99923@gmail.com>
2024-01-15 00:18:40 +0530

    Auto generate continue.

--- src/Services/SettingServices/AutoNumberSeries.service.ts
+++ src/Services/SettingServices/AutoNumberSeries.service.ts
@@ -117,38 +117,68 @@ class AutoNumberGenerationService {
       (autoNumber) => autoNumber.entityType === entity_type,
     );
 
+    let usableTransactionNumber: string;
+
     // get the current sequence
     const currentNumber = autoNumber.nextNumber;
     const currentPrefix = autoNumber.prefixString;
-    const currentTransactionNumber = currentPrefix + currentNumber;
+    const currentTransactionNumber = this.#formatNextTransactionNumber(
+      currentPrefix,
+      currentNumber,
+    );
 
-    // note: if even after generating the next sequence, it already exists,
-    // we throw an error, because we can't handle that situation
+    // check if the current sequence is already used
     const currentTransactionNumberAlreadyExists = await this.#checkIfExists(
       entity_type,
       currentTransactionNumber,
     );
-    if (currentTransactionNumberAlreadyExists) {
-      switch (entity_type) {
-        case "invoice":
-          throw new CodedError(
-            AutoNumberSeriesServiceErrorMessages.INVOICE_NUMBER_ALREADY_EXISTS,
-          );
-        case "credit_note":
-          throw new CodedError(
-            AutoNumberSeriesServiceErrorMessages.CREDIT_NOTE_NUMBER_ALREADY_EXISTS,
-          );
-        case "customer_payment":
-          throw new CodedError(
-            AutoNumberSeriesServiceErrorMessages.CREDIT_NOTE_NUMBER_ALREADY_EXISTS,
-          );
-      }
+    if (currentTransactionNumberAlreadyExists === false) {
+      usableTransactionNumber = currentTransactionNumber;
     }
 
     // generate the next sequence by incrementing the current sequence by 1
-    const nextNumber = (parseInt(currentNumber) + 1)
-      .toString()
-      .padStart(autoNumber.numberZeroPad, "0");
+    let nextNumber = this.#formatNextNumber(
+      parseInt(currentNumber) + 1,
+      autoNumber.numberZeroPad,
+    );
+
+    // if the current sequence is already used, try to use the next sequence (and generate another next sequence)
+    // although we will throw an error if the next sequence is also used
+    if (currentTransactionNumberAlreadyExists) {
+      const nextTransactionNumberAlreadyExists = await this.#checkIfExists(
+        entity_type,
+        this.#formatNextTransactionNumber(currentPrefix, nextNumber),
+      );
+
+      // throw an error if the next sequence is also used
+      if (nextTransactionNumberAlreadyExists) {
+        switch (entity_type) {
+          case "invoice":
+            throw new CodedError(
+              AutoNumberSeriesServiceErrorMessages.INVOICE_NUMBER_ALREADY_EXISTS,
+            );
+          case "credit_note":
+            throw new CodedError(
+              AutoNumberSeriesServiceErrorMessages.CREDIT_NOTE_NUMBER_ALREADY_EXISTS,
+            );
+          case "customer_payment":
+            throw new CodedError(
+              AutoNumberSeriesServiceErrorMessages.CREDIT_NOTE_NUMBER_ALREADY_EXISTS,
+            );
+        }
+      }
+
+      usableTransactionNumber = this.#formatNextTransactionNumber(
+        currentPrefix,
+        nextNumber,
+      );
+
+      // now we generate another next number
+      nextNumber = this.#formatNextNumber(
+        parseInt(nextNumber) + 1,
+        autoNumber.numberZeroPad,
+      );
+    }
 
     // update the auto number
     await this._autoNumberDAO.update(
@@ -162,7 +192,15 @@ class AutoNumberGenerationService {
         transaction,
       },
     );
-    return currentTransactionNumber;
+    return usableTransactionNumber;
+  }
+
+  #formatNextNumber(number_start_at: number, number_zero_pad: number) {
+    return number_start_at.toString().padStart(number_zero_pad, "0");
+  }
+
+  #formatNextTransactionNumber(prefix: string, next_number: string) {
+    return prefix + next_number;
   }
 
   async #checkIfExists(
